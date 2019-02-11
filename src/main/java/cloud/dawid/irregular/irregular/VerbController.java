14f2c489a7eba4b021cdf2911f91f456fe11c360
Dawid Honkisz <dhonkisz@seberus.local>
2019-02-11 23:50:34 +0100

    add en to pl

--- src/main/java/cloud/dawid/irregular/irregular/VerbController.java
+++ src/main/java/cloud/dawid/irregular/irregular/VerbController.java
@@ -36,12 +36,21 @@ public class VerbController {
                                         ) {
         Optional<Verb> oneverb = verbService.getOneByID(verb);
 
-        //jeżeli jest błędna odpowiedz to dodajemy info
+        if(verb < 100){
+            int nextverb = verb + 1;
+            model.addAttribute("nextverb", nextverb);
+        }else{
+            model.addAttribute("nextverb", "last");
+        }
+
+
+        //jeżeli jest błędna odpowiedz to dodajemy info o tym
         if(!error.isEmpty()){
             model.addAttribute("error", error);
             model.addAttribute("infinitive", infinitive);
             model.addAttribute("pastsimple", pastsimple);
             model.addAttribute("pastparticiple", pastparticiple);
+
         }else if(!info.isEmpty()){
             model.addAttribute("info", info);
         }
@@ -73,25 +82,96 @@ public class VerbController {
         if(!infinitive.equals(infinitiveGood)){
             redirectAttributes.addFlashAttribute("infinitive" , "error");
             correct = false;
+        }else{
+            redirectAttributes.addFlashAttribute("infinitive" , "correct");
         }
         if(!pastsimple.equals(pastsimpleGood)){
             redirectAttributes.addFlashAttribute("pastsimple", "error");
             correct = false;
+        }else {
+            redirectAttributes.addFlashAttribute("pastsimple", "correct");
         }
         if(!pastparticiple.equals(pastparticipleGood)){
             redirectAttributes.addFlashAttribute("pastparticiple", "error");
             correct = false;
+        }else {
+            redirectAttributes.addFlashAttribute("pastparticiple", "correct");
         }
 
         if(correct) {
             redirectAttributes.addFlashAttribute("info", "poprawna");
             redirectAttributes.addFlashAttribute("error", "no");
-            int nextverb = ++idverb;
-            return "redirect:/l/pl/"+nextverb;
+            return "redirect:/l/pl/"+idverb;
 
         }else{
             redirectAttributes.addFlashAttribute("error", "yes");
             return "redirect:/l/pl/"+idverb;
+        }
+    }
+
+
+
+    /*wersja angielski na polski*/
+    @RequestMapping(value = "/l/en/{verb}", method = RequestMethod.GET)
+    public String showOneVerbsAndFormPL(Model model, @PathVariable("verb") int verb,
+                                        @ModelAttribute String tlumaczenie,
+                                        @ModelAttribute String error,
+                                        @ModelAttribute String info
+    ) {
+        Optional<Verb> oneverb = verbService.getOneByID(verb);
+
+        if(verb < 100){
+            int nextverb = verb + 1;
+            model.addAttribute("nextverb", nextverb);
+        }else{
+            model.addAttribute("nextverb", "last");
+        }
+
+
+        //jeżeli jest błędna odpowiedz to dodajemy info o tym
+        if(!error.isEmpty()){
+            model.addAttribute("error", error);
+            model.addAttribute("tlumaczenie", tlumaczenie);
+
+        }else if(!info.isEmpty()){
+            model.addAttribute("info", info);
+        }
+
+        model.addAttribute("verb", oneverb.get());
+        return "verblen";
+    }
+
+
+    @RequestMapping(value = "/l/en/check/", method = RequestMethod.POST)
+    public String checkOnePL(Model model,
+                             RedirectAttributes redirectAttributes,
+                             @RequestParam("idverb") int idverb,
+                             @RequestParam("tlumaczenie") String tlumaczenie)
+    {
+        //konwersja dużych liter na małe
+        tlumaczenie = tlumaczenie.toLowerCase();
+
+        //Pobranie poprawnej odpowiedzi i przypisanie do Stringów
+        Optional<Verb> optionalVerb = verbService.getOneByID(idverb);
+        String tlumaczenieGood = optionalVerb.get().getTlumaczenie();
+
+        boolean correct = true;
+
+        if(!tlumaczenie.equals(tlumaczenieGood)){
+            redirectAttributes.addFlashAttribute("tlumaczenie" , "error");
+            correct = false;
+        }else{
+            redirectAttributes.addFlashAttribute("tlumaczenie" , "correct");
+        }
+
+        if(correct) {
+            redirectAttributes.addFlashAttribute("info", "poprawna");
+            redirectAttributes.addFlashAttribute("error", "no");
+            return "redirect:/l/en/"+idverb;
+
+        }else{
+            redirectAttributes.addFlashAttribute("error", "yes");
+            return "redirect:/l/en/"+idverb;
         }
     }
 }
